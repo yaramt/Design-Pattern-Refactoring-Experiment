bbfa27e2ee0dc6ed75a8ff843abab07360bd43f8
yaramt <mataniyara@gmail.com>
2024-02-07 19:55:42 +0100

    fix hmvc ShoppingCartView

--- app/src/main/java/org/ecommerce/hmvc/mvcShoppingCart/view/ShoppingCartView.java
+++ app/src/main/java/org/ecommerce/hmvc/mvcShoppingCart/view/ShoppingCartView.java
@@ -1,14 +1,18 @@
 package org.ecommerce.hmvc.mvcShoppingCart.view;
 
 import org.ecommerce.hmvc.mvcProduct.model.ProductModel;
+import org.ecommerce.hmvc.mvcProduct.view.ProductView;
 
 import java.util.List;
 
 public class ShoppingCartView {
+    private ProductView productView = new ProductView();
+
     public void displayCart(List<ProductModel> items) {
         System.out.println("Shopping Cart Items:");
         for (ProductModel item : items) {
-            System.out.println(item.getName() + " - $" + item.getPrice());
+            productView.displayProduct(item);
         }
     }
 }
+
