27d15173a82203624c017e39882f2a92b35b4321
yaramt <mataniyara@gmail.com>
2024-03-14 19:04:59 +0100

    Add more methods to shoppingcart patternless

--- app/src/main/java/org/ecommerce/patternless/ShoppingCart.java
+++ app/src/main/java/org/ecommerce/patternless/ShoppingCart.java
@@ -2,25 +2,37 @@ package org.ecommerce.patternless;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public class ShoppingCart {
-    List<String> items = new ArrayList<>();
+    List<String> products = new ArrayList<>();
+
     public void addItem(String product) {
-        items.add(product);
+        products.add(product);
     }
+
     public void showItems() {
-        for (String item : items) {
-            System.out.println(item);
+        for (String product : products) {
+            System.out.println(product);
         }
     }
+
+    public void removeItem(String product) {
+         products.remove(product);
+    }
+
+    public List<String> getProducts() {
+            return products;
+    }
+
     public static void main(String[] args) {
         String laptop = "Laptop - $999.99";
         String headphones = "Headphones - $49.99";
-
         ShoppingCart cart = new ShoppingCart();
         cart.addItem(laptop);
         cart.addItem(headphones);
-
         cart.showItems();
     }
 }
+
+
 
